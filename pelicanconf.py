90cb337fb55edf1c2ab37a02936434f04083bd9f
Lumy <lumy@lumy.me>
2019-11-17 02:14:37 +0100

    config and theme update

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,12 +11,11 @@ PATH='content'
 LOAD_CONTENT_CACHE = False
 CACHE_CONTENT = False
 
-CATEGORY_URL = '{slug}/'
-CATEGORY_SAVE_AS = '{slug}/index.html'
+CATEGORY_URL = '{slug}/category'
+CATEGORY_SAVE_AS = '{slug}/category.html'
 
-ARTICLE_URL = '{category}/{slug}/'
+ARTICLE_URL = '{lang}/{category}/{slug}/'
 ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
-
 
 TIMEZONE = 'Europe/Paris'
 
@@ -30,22 +29,18 @@ THEME = "pelican-themes/brownstone"
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = [
     "jinja2content",
-    "i18n_subsites",
     "libravatar",
+    "pelicanfly",
+    "i18n_subsites",
 ]
 
 LIBRAVATAR_AUTHOR_EMAIL="lumy@lumy.me"
 LIBRAVATAR_SIZE=220
-TEST_LUMY="TEST"
 
 I18N_SUBSITES = {
-    'en': {
-        'SITENAME': 'Lumy\'s blog',
-        'THEME_STATIC_PATHS': ["themes/",]
-    },
     'fr': {
         'SITENAME': 'Journal de Lumy',
-        'THEME_STATIC_PATHS': ["themes/",]
+        'THEME_STATIC_PATHS': ["themes/",],
     }
 }
 
@@ -57,11 +52,6 @@ DISPLAY_PAGES_ON_MENU = True
 # Whether to display categories on the menu of the template.
 DISPLAY_CATEGORIES_ON_MENU = True
 
-# DEFAULT_METADATA = {
-#   'description': 'A brief description of your site',
-#   'status': 'draft'
-# }
-
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -70,15 +60,17 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 TYPOGRIFY = True
 # Blogroll
-LINKS = (('www.lumy.me', 'http://www.lumy.me/'),
-         ('cv.lumy.me', 'http://cv.lumy.me/'),
-         ('github', 'https://github.com/lumy'),
-         ('Music', ''),
-)
+LINKS = (("Lta's Blog", 'http://blog.lta.io/'),
+         ("Lumy's website", 'https://www.lumy.me'),
+         ("Lumy's cv", 'http://cv.lumy.me/'),
+        ("Rohja's Blog", "https://blog.rohja.com/"),
+) 
 
 # Social widget
-SOCIAL = (
-    ('Twitter', 'https://twitter.com/Lumy4242'),
+MSOCIAL = (
+    ('Twitter', 'https://twitter.com/Lumy4242', "fab fa-2x fa-twitter"),
+    ('Github', 'https://github.com/lumy', "fab fa-2x fa-github"),
+    ('Music', 'https://soundcloud.com/lumyi', "fab fa-2x fa-soundcloud"),
 )
 
 DEFAULT_PAGINATION = 10
@@ -89,9 +81,30 @@ RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
 
 def extract_trans(article, lang, url):
-    for trans in article.translations:
-        if trans.lang == lang:
-            return trans.url
-        return url
+  for trans in article.translations:
+    if trans.lang == lang:
+        return trans.url
+    return url
 
-JINJA_FILTERS = {'extract_trans': extract_trans}
+languages_lookup = {
+                 'en': 'English',
+                 'fr': 'Francais',
+                 }
+def lookup_lang_name(lang_code):
+  return languages_lookup[lang_code]
+
+import os
+
+def generate_url_lang(lang_file):
+  lang, filename = lang_file[0], lang_file[1]
+  if lang != "en":
+    return os.path.join("/", lang, filename)
+  else:
+    return os.path.join("/", filename)
+
+JINJA_FILTERS = {
+    'extract_trans': extract_trans,
+    'lookup_lang_name': lookup_lang_name,
+  "generate_url_lang": generate_url_lang,
+}
+TWITTER_USERNAME="lumy4242"
