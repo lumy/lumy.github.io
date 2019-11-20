6bee06c6bf4cd9590dfe7ab073ae17737ef590a7
Lumy <lumy@lumy.me>
2019-11-20 23:52:22 +0100

    public update with cname

--- publishconf.py
+++ publishconf.py
@@ -11,11 +11,13 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://lumy.github.io' # https://blog.lumy.me
+SITEURL = 'https://blog.lumy.me'
+STATIC_PATHS = ["images", "extra/CNAME"]
+EXTRA_PATH_METADATA = { 'extra/CNAME': {'path': 'CNAME'}}
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
