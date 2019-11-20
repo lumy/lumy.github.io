df6b0fde9322a28a62cf107ae89083a3058b665a
Lumy <lumy@lumy.me>
2019-11-21 00:28:33 +0100

    nik

--- temp.sh
+++
@@ -1,8 +0,0 @@
-
-for i in `ls pelican-themes`; do
-		echo "Testing $i"
-		sed -i "s/THEME=.*/THEME='pelican-themes\/$i'/g" pelicanconf.py 2> /dev/null
-		make clean
-		make html || exit
-		read
-done
